6b44f6b03c66cc5868b824db5e7a1b0d97cf312e
Suresh Natarajan <suresh.n.natarajan@gmail.com>
2020-05-02 23:16:51 +0530

    Added web security and allowed origins

--- src/main/java/com/vaaniga/invest/signup/VaanigaInvestSignupApplication.java
+++ src/main/java/com/vaaniga/invest/signup/VaanigaInvestSignupApplication.java
@@ -2,12 +2,25 @@ package com.vaaniga.invest.signup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class VaanigaInvestSignupApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VaanigaInvestSignupApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/signUp").allowedOrigins("https://vaaniga-invest-app.herokuapp.com/");
+			}
+		};
 	}
 
 }
