821bf75e25ff501197125c71939b66ee4515091d
Suresh Natarajan <suresh.n.natarajan@gmail.com>
2020-05-02 22:58:37 +0530

    Added spring security dependancies

--- src/main/java/com/vaaniga/invest/signup/controller/VaanigaSignupController.java
+++ src/main/java/com/vaaniga/invest/signup/controller/VaanigaSignupController.java
@@ -1,11 +1,18 @@
 package com.vaaniga.invest.signup.controller;
 
+import java.util.Collections;
+import java.util.Map;
+
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +26,7 @@ import com.vaaniga.invest.signup.service.VaanigaSignupService;
 
 @RestController
 @RequestMapping(path="/signUp")
-@CrossOrigin("*")
+//@CrossOrigin("*")
 public class VaanigaSignupController {
 
 	private VaanigaSignupService vaanigaSignupService;
@@ -30,6 +37,7 @@ public class VaanigaSignupController {
 		this.vaanigaSignupService = vaanigaSignupServiceParam;
 	}
 
+	@CrossOrigin(origins = "https://vaaniga-invest-app.herokuapp.com/")
 	@PostMapping(value = "/", produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Object> signupUser(@RequestBody SignupRequestDto signupRequestDto ) throws SignupServiceGenericException {
 		
@@ -47,19 +55,16 @@ public class VaanigaSignupController {
 		return new ResponseEntity<>(responseObject.toMap(), HttpStatus.OK);
 	}
 	
-	/*
-	//@CrossOrigin(origins = "https://vaaniga-invest-app.herokuapp.com/")
+	@CrossOrigin(origins = "https://vaaniga-invest-app.herokuapp.com/")
 	@GetMapping("/user")
     public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
         return Collections.singletonMap("name", principal.getAttribute("name"));
     }
 	
-	//@CrossOrigin(origins = "https://vaaniga-invest-app.herokuapp.com/")
+	@CrossOrigin(origins = "https://vaaniga-invest-app.herokuapp.com/")
 	@GetMapping("/social/{appName}")
 	public String redirectToSocial(@PathVariable String appName) {
 		
 		return "redirect:"+vaanigaSignupService.constructSocialRedirection(appName);
 	}
-	*/
 }
-
